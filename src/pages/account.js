127c62d8171d1ab24bf1b18bfc7e2927307a0075
Alasow <calasow029@hotmail.com>
2023-12-21 16:41:55 +0300

    account

--- src/pages/account.js
+++ src/pages/account.js
@@ -3,6 +3,7 @@ import {Router} from '@reach/router'
 import { login} from './utils/auth'
 const Home = () => <p>Home</p>
 const Settings = () => <p>Settings</p>
+const Logout = () => <p>Logout</p>
 const Billing = () => <p>Billing</p>
 
 
@@ -11,6 +12,8 @@ const Account = () => {
    return (
     <Router>
  <Settings path="/account/settings" />
+ <Logout path="/account/logout" />
+
  <Billing path="/account/billing" />
  <Home path="/account/" />
     </Router>
